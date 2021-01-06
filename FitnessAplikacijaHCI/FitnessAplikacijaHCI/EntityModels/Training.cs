5a046831465b5b0c64dece70282690f960c2b144
Elma Škulj <elmaskulj@users.noreply.github.com>
2021-01-06 13:48:43 +0100

    prvi commit

---
+++ FitnessAplikacijaHCI/FitnessAplikacijaHCI/EntityModels/Training.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FitnessAplikacijaHCI.EntityModels
+{
+    public class Training
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public string Opis { get; set; }
+
+
+    }
+}
